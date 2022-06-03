607daa9d9b3bbbfa57b37d6531f6fd754ffad509
Gpcc88 <90462736+Gpcc88@users.noreply.github.com>
2022-06-03 16:49:38 +0100

    Parte 1

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        //You can omit the DbSet<Enrollment> and DbSet<Course> statements
+        //and it would work the same. Entity Framework would include them
+        //implicitly because the Student entity references the Enrollment
+        //entity and the Enrollment entity references the Course entity.
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //prevents table names from being pluralized.
+        }
+    }
+}
