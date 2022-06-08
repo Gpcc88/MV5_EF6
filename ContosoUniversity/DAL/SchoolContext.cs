61280274af559d7f69e5f242e9c441d9cb6059c5
Gpcc88 <90462736+Gpcc88@users.noreply.github.com>
2022-06-08 14:58:25 +0100

    Final

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -2,28 +2,31 @@
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
+
 namespace ContosoUniversity.DAL
 {
+
     public class SchoolContext : DbContext
     {
 
-        public SchoolContext() : base("SchoolContext")
-        {
-        }
-
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
-
-        //You can omit the DbSet<Enrollment> and DbSet<Course> statements
-        //and it would work the same. Entity Framework would include them
-        //implicitly because the Student entity references the Enrollment
-        //entity and the Enrollment entity references the Course entity.
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-            //prevents table names from being pluralized.
+
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                    .MapRightKey("InstructorID")
+                    .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
